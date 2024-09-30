b4061779ffa920b16512b421117877e2f023f5cf
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-09-30 13:29:54 +0200

    Added in basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     reactStrictMode: true,
     swcMinify: true,
     output: 'export',
     images: {
-        unoptimized: true
+        loader: 'akamai',
+        path: '/'
     },
 };
 
